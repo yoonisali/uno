c494bb0ee503f2100f2eba7fc6cd5d36ec91d7e0
Robert Onstott <roonstott@gmail.com>
2022-11-28 13:46:57 -0800

    move code out of index.js into business.js file
    co-authored-by: Yoonis Ali <yoonismahamoudali@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,79 +1,14 @@
+import Game from './js/business.js'
 import './css/styles.css';
+import "bootstrap";
+import * as $ from "jquery";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-class Game {
-  constructor(humanName) {
-    this.humanTurn = true;
-    this.uidCounter = 0;
-    this.bot = {
-      hand: this.makeHand()
-    };
-    this.human = {
-      name: humanName,
-      hand: this.makeHand()
-    };
-  }
+$(".opponent-card").html(`
+    <div class="card-body">
+        <h5 class="card-title">Uno</h5>
+    </div>
+`);
 
-  makeHand() {
-    let hand = [];
-    for (let i = 0; i < 7; i++) {
-      const card = this.randomCard();
-      hand.push(card);
-    }
-    return hand;
-  }
-
-
-  randomCard() {
-    const randomNumber = Math.floor(Math.random() * 14);
-    const wildRandom = Math.floor(Math.random() * 2);
-    const colorRandom = Math.floor(Math.random() * 4);
-    const possibleValues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "skip", "reverse", "+2", "wild"];
-    let value = possibleValues[randomNumber];
-    if (value === "wild" && wildRandom === 1) {
-      value = "wild4";
-    }
-    const possibleColors = ["red", "blue", "green", "yellow"];
-    let color = possibleColors[colorRandom];
-    if (value === "wild" || value === "wild4") {
-      color = null;
-    }
-    this.uidCounter++;
-    return {
-      color: color,
-      value: value,
-      uid: this.uidCounter
-    };
-  }
-  
-
-  changeTurn() {
-    let turn = this.humanTurn;
-    if (turn === true) {
-      turn = false;
-    } else if (turn === false) {
-      turn = true;
-    }
-  }
-
-}
-
-// Console Testing -------------------------------
 let game = new Game("Yoonis");
 console.log(game);
-
-
-
-// function Player(human, bot) {
-//   this.human = human;
-//   this.bot = bot;
-// }
-
-
-
-// Player.prototype.assignTurn = function () {
-//   // let human = this.human
-//   // let bot = this.bot;
-//   // let playerTurn = true;
-//   // let botPlayers = [bot1, bot2, bot3];
-// };
-
